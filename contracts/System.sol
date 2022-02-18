3e4e84b7469e3517f92f29d380126990f1dddb3d
BigPingge <1095205711@qq.com>
2022-02-18 17:32:04 +0800

    测试网部分配置

--- contracts/System.sol
+++ contracts/System.sol
@@ -16,7 +16,12 @@ contract System {
   bytes public constant INIT_VALIDATORSET_BYTES = hex"f84580f842f84094f9056de9c0c6e8fC3097c3612110641190e0C37b94f9056de9c0c6e8fC3097c3612110641190e0C37b94f9056de9c0c6e8fC3097c3612110641190e0C37b64";
 
   //创世节点初始化验证
-  address public constant GENESIS_NODE = 0xf9056de9c0c6e8fC3097c3612110641190e0C37b;
+  address public constant GENESIS_NODE1 = 0xEe7F0D15bAce58877fBcCe21442d7f8C271a162E;
+  address public constant GENESIS_NODE2 = 0x969fb73d144672a22680A844854dF0e1999eA573;
+  address public constant GENESIS_NODE3 = 0x9fF4E2D26b4675d91e07BfB04E16DB35a4337b4e;
+  address public constant GENESIS_NODE4 = 0xc65380E428F994f436A8838C6aF06Fd2ba01E2a4;
+
+  address public constant GENESIS_ADMIN = 0x735989886643ec18c215008516C709Ee2606e225;
 
   modifier onlyCoinbase() {
     require(msg.sender == block.coinbase, "the message sender must be the block producer");
